755a7958285a66456968381cf6a83e5dc8452386
Vachaspathy Kuntamukkala <vachaspathy@gmail.com>
2019-02-07 12:25:16 -0800

    Update new_macos_essentials.sh

--- new_macos_essentials.sh
+++ new_macos_essentials.sh
@@ -9,7 +9,6 @@ brew tap phinze/homebrew-cask
 brew tap homebrew/dupes
 brew tap caskroom/versions
 
-brew install homebrew/completions/brew-cask-completion
 brew install brew-cask
 
 brew cask install google-chrome
